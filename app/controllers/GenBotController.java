2cf6a27ae2d1138eb484cab1448a7629f00a9f27
petres <peter.reschenhofer@gmail.com>
2013-11-10 19:55:42 +0100

    add missing files

---
+++ app/controllers/GenBotController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.error;
+
+public class GenBotController extends Controller {
+	protected static Result error(Exception e) {
+		System.out.println("EXCEPION " + e.getMessage() + " (" + e.getClass() + ")");
+		return ok(error.render(e));
+	}
+	protected static Result errorAjax(Exception e) {
+		ObjectNode result = Json.newObject();
+		result.put("valid", false);
+		result.put("error", "EXCEPION " + e.getMessage() + " (" + e.getClass() + ")");
+		return ok(result);
+	}
+}
---
+++ app/controllers/Serial.java
@@ -0,0 +1,100 @@
+package controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import serialRMI.*;
+import play.libs.Json;
+//import play.*;
+//import play.cache.Cache;
+import play.mvc.*;
+import views.html.*;
+
+import java.rmi.*;
+
+import models.GenBotProtocol;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class Serial extends GenBotController {
+	private static String serialRMIServer = "//127.0.0.1:12121/serial";
+	private static SerialRMIInterface genBotSerialRMIConnect() throws Exception {
+		return (SerialRMIInterface) Naming.lookup(session("serialRMIServer"));
+	}
+	
+	public static Result index() {
+		if(session("serialRMIServer") == null)
+			session("serialRMIServer", serialRMIServer);
+		try {
+			try {
+				SerialRMIInterface serialRMI = genBotSerialRMIConnect();
+				if(serialRMI.isConnected()) {
+					return ok(serial.render());
+				} else {
+					return ok(serialConnect.render(serialRMI.getSerialPorts(), session("serialRMIServer")));
+				}
+			} catch (java.rmi.ConnectException e) {
+				return ok(serialConnect.render(null,session("serialRMIServer")));
+			}
+		} catch (Exception e) {
+			return error(e);
+		}
+	}
+	
+	public static Result connect() {
+		Map<String, String[]> parameters = request().body().asFormUrlEncoded();
+		if(parameters.containsKey("rmiServer"))
+			session("serialRMIServer", parameters.get("rmiServer")[0]);
+		
+		try {
+			SerialRMIInterface serialRMI = genBotSerialRMIConnect();
+			serialRMI.connect(parameters.get("port")[0]);
+			return redirect("/serial");
+		} catch (Exception e) {
+			return error(e);
+		}
+	}
+
+	public static Result write() {
+		Map<String, String[]> parameters = request().body().asFormUrlEncoded();
+		try {
+			SerialRMIInterface serialRMI = genBotSerialRMIConnect();
+			ObjectNode result = Json.newObject();
+			String text = parameters.get("text")[0] + "\r\n";
+			serialRMI.write(text);
+			result.put("valid", true);
+			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+			result.put("timestamp", sdf.format(new Date().getTime()));
+			result.put("string", text.replaceAll("\\r\\n", "\\\\r\\\\n"));
+			return ok(result);
+		} catch (Exception e) {
+			return error(e);
+		}
+	}
+
+	public static Result read() {
+		try {
+			SerialRMIInterface serialRMI = genBotSerialRMIConnect();
+			ObjectNode result = Json.newObject();
+			String str = serialRMI.read();
+			result.put("valid", true);
+			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+			result.put("timestamp", sdf.format(new Date().getTime()));
+			if(str.length() > 0) {
+				result.put("string", str.replaceAll("\\r\\n", "\\\\r\\\\n"));
+				
+				//GenBotProtocol.getInstance().read(str);
+				for (GenBotProtocol.Message m: GenBotProtocol.getInstance().read(str)) {
+					System.out.println("Command: " + m.command);
+				}
+			} else {
+				result.put("string", "");
+			}
+			
+			return ok(result);
+		} catch (Exception e) {
+			return errorAjax(e);
+		}
+	}
+}
