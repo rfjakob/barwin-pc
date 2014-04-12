1d31fa33fc4728b3b606b3b8aa8b9ae3a4f8b9b4
Jakob Unterwurzacher <jakobunt@gmail.com>
2014-04-12 21:48:04 +0200

    genBotWI: Change to generic play framework path
    /opt/play should be symlinked to a specific version

--- genBotWI/run.sh
+++ genBotWI/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/play/play-2.2.1/play run 80 -Djava.security.manager -Djava.security.policy=:/opt/roboexotica/genBot2/bin/genBot2/my.policy
+/opt/play/play run 80 -Djava.security.manager -Djava.security.policy=:../genBot2/bin/genBot2/my.policy
