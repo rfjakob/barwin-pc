51695294733e2765219a7029e8adbffa794023bc
petres <peter.reschenhofer@gmail.com>
2014-04-27 18:08:55 +0200

    removing sqlite

--- bin/genBot.sh
+++ bin/genBot.sh
@@ -3,4 +3,4 @@
 # Make sure we are in the right directory
 cd "$(dirname "$(realpath "$0")")";
 
-java -cp ../lib/genBot.jar:../lib/serialRMI.jar:../lib/sqlite-jdbc.jar genBot.RMIServer
+java -cp ../lib/genBot.jar:../lib/serialRMI.jar genBot.RMIServer
